5decf3e1b76decd4c9ed54ac72367347a30504a0
Rintaro Hirokawa <rintarohirokawa@gmail.com>
2019-06-25 16:51:51 +0900

    new commit!

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     sys.argv += ['--style', 'material']
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(QUrl("view/view_2.qml"))
-    # engine.load(QUrl("sdtest_ui_test.qml"))
+    engine.load(QUrl("sdtest_ui_test.qml"))
+    # engine.load(QUrl("view/view_2.qml"))
+
 
     if not engine.rootObjects():
         sys.exit(-1)
